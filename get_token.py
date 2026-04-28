aab6a2afdae37ce99c9d0aaa71966a2e8ef4c9d5
salesroast <sales@cafeinmaculada.com>
2026-04-28 10:29:00 -0500

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -8,10 +8,6 @@ SHOP = os.environ.get("SHOPIFY_SHOP_DOMAIN")
 CLIENT_ID = os.environ.get("SHOPIFY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SHOPIFY_CLIENT_SECRET")
 
-print("SHOP:", SHOP)
-print("CLIENT_ID:", CLIENT_ID)
-print("SECRET cargado:", bool(CLIENT_SECRET))
-
 url = f"https://{SHOP}/admin/oauth/access_token"
 payload = {
     "client_id": CLIENT_ID,
